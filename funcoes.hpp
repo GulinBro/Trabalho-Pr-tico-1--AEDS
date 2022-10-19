22f6f7e6c7031c7cdf1173e5e1f86e3db801c786
Gustavo Brendon <104071330+GulinBro@users.noreply.github.com>
2022-10-19 07:24:51 -0300

    Create funcoes.hpp

---
+++ funcoes.hpp
@@ -0,0 +1,11 @@
+#ifndef FUNCOES_H
+#define FUNCOES_H
+
+#include "listaEncadeada.hpp"
+
+void menu();
+void recuperaLista(ListaFuncionario *lista);
+void salvaLista(ListaFuncionario *lista);
+void desalocaLista(ListaFuncionario *lista);
+
+#endif
